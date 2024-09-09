204978c5f8e4e01d92394b40e7fc198f122f7a0e
Челомов Степан <stepan.chelomov@mail.ru>
2024-09-09 22:56:57 +0300

    change testing

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -1,17 +1,18 @@
 import random
 
-count_tests = 10
+tests_number = 10
 
-for test_number in range(0, count_tests):
+for test_number in range(0, tests_number):
     name_of_file = "test_" + f'{test_number + 1:02}' + ".in"
     file = open(name_of_file, 'w')
 
-    size_of_cache  = random.randint(10000, 20000)
-    count_of_elems = random.randint(100000, 200000)
+    cache_capacity  = random.randint(1000, 2000)
 
-    test_text = str(size_of_cache) + " " + str(count_of_elems) + "\n"
+    elems_number = random.randint(100000, 200000)
 
-    for elem_number in range(count_of_elems) :
+    test_text = str(cache_capacity) + " " + str(elems_number) + "\n"
+
+    for elem_number in range(elems_number) :
         test_text += str(random.randint(0, 100000)) + " "
 
     file.write(test_text)
